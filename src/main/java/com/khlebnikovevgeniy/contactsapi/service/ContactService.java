a63c9fa84ca94634b877c26e6b947cb347131e4d
KhlebnikovEvgeniy <khebnikove@gmail.com>
2025-03-12 19:16:35 +0200

    Contact Service

--- src/main/java/com/khlebnikovevgeniy/contactsapi/service/ContactService.java
+++ src/main/java/com/khlebnikovevgeniy/contactsapi/service/ContactService.java
@@ -1,7 +1,9 @@
 package com.khlebnikovevgeniy.contactsapi.service;
 
-import java.awt.Image;
-import java.io.File;
+import static com.khlebnikovevgeniy.contactsapi.constant.Constant.CONTACTS_IMAGE_ENDPOINT;
+import static com.khlebnikovevgeniy.contactsapi.constant.Constant.PHOTO_DIRECTORY;
+import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
+
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -23,34 +25,31 @@ import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
-import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
-import static com.khlebnikovevgeniy.contactsapi.constant.Constant.PHOTO_DIRECTORY;
-import static com.khlebnikovevgeniy.contactsapi.constant.Constant.CONTACTS_IMAGE_ENDPOINT;
 
 @Service
 @Slf4j
 @Transactional(rollbackOn = Exception.class)
 @RequiredArgsConstructor
 public class ContactService {
-	private final ContactRepo contactRepo;
-	
-	public Page<Contact> getAllCoctacts(int page, int size) {
-		return contactRepo.findAll(PageRequest.of(page, size, Sort.by("name")));
-	}
-	
-	public Contact getContact(String id) {
-		return contactRepo.findById(id).orElseThrow(() -> new RuntimeException("Contact not found"));
-	}
-	
-	public Contact createContact(Contact contact) {
-		return contactRepo.save(contact);
-	}
-	
-	public void deleteContact(Contact contact) {
-		contactRepo.delete(contact);
-	}
-	
-	public String uploadPhoto(String id, MultipartFile file) {
+    private final ContactRepo contactRepo;
+
+    public Page<Contact> getAllContacts(int page, int size) {
+        return contactRepo.findAll(PageRequest.of(page, size, Sort.by("name")));
+    }
+
+    public Contact getContact(String id) {
+        return contactRepo.findById(id).orElseThrow(() -> new RuntimeException("Contact not found"));
+    }
+
+    public Contact createContact(Contact contact) {
+        return contactRepo.save(contact);
+    }
+
+    public void deleteContact(Contact contact) {
+        contactRepo.delete(contact);
+    }
+
+    public String uploadPhoto(String id, MultipartFile file) {
         log.info("Saving picture for user ID: {}", id);
         Contact contact = getContact(id);
         String photoUrl = photoFunction.apply(id, file);
@@ -58,11 +57,11 @@ public class ContactService {
         contactRepo.save(contact);
         return photoUrl;
     }
-	
-	private final Function<String, String> fileExtension = filename -> Optional.of(filename).filter(name -> name.contains("."))
+
+    private final Function<String, String> fileExtension = filename -> Optional.of(filename).filter(name -> name.contains("."))
             .map(name -> "." + name.substring(filename.lastIndexOf(".") + 1)).orElse(".png");
-	
-	private final BiFunction<String, MultipartFile, String> photoFunction = (id, image) -> {
+
+    private final BiFunction<String, MultipartFile, String> photoFunction = (id, image) -> {
         String filename = id + fileExtension.apply(image.getOriginalFilename());
         try {
             Path fileStorageLocation = Paths.get(PHOTO_DIRECTORY).toAbsolutePath().normalize();
