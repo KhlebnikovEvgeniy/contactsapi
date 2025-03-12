d45b4058fcbeddc112e599fca15c98cddce0bfe1
KhlebnikovEvgeniy <khebnikove@gmail.com>
2025-03-12 19:31:04 +0200

    Constant

--- src/main/java/com/khlebnikovevgeniy/contactsapi/constant/Constant.java
+++ src/main/java/com/khlebnikovevgeniy/contactsapi/constant/Constant.java
@@ -3,5 +3,5 @@ package com.khlebnikovevgeniy.contactsapi.constant;
 public class Constant {
 	public static final String PHOTO_DIRECTORY = System.getProperty("user.home") + "/Downloads/uploads/";
 	public static final String CONTACTS_IMAGE_ENDPOINT = "/contacts/image/";
-
+	public static final String X_REQUESTED_WITH = "X-Requested-With";
 }
